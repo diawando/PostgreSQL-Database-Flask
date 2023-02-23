bc08d55ecaf5a0d31106885d6ff5429928c1730a
diawando <abduldiawade777@gmail.com>
2023-02-23 23:47:18 +0000

    mis à jour de app.py

--- init_db.py
+++ init_db.py
@@ -18,7 +18,7 @@ cur.execute('CREATE TABLE books(id serial PRIMARY KEY,'
             'author varchar (50) NOT NULL,'
             'pages_num integer NOT NULL,'
             'review text,'
-            'date_added date DEFAULT CURRENT_TIME);'
+            'date_added date DEFAULT CURRENT_TIMESTAMP);'
             )
 
 # Insert data into the table
@@ -30,6 +30,15 @@ cur.execute('INSERT INTO books(title, author, pages_num, review)'
              489,
              'A great classic!')
             )
+
+cur.execute('INSERT INTO books (title, author, pages_num, review)'
+            'VALUES (%s, %s, %s, %s)',
+            ('Anna Karenina',
+             'Leo Tolstoy',
+             864,
+             'Another great classic')
+            )
+
 
 conn.commit()
 
